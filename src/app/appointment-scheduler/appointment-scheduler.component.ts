b9b37b39c2eb451e2213de4ff34b8ede2589b1c3
Ole Eskild Steensen <oleeskilds@gmail.com>
2018-08-19 17:39:22 +0200

    Disabled dates should work

--- src/app/appointment-scheduler/appointment-scheduler.component.ts
+++ src/app/appointment-scheduler/appointment-scheduler.component.ts
@@ -4,6 +4,7 @@ import {ActivatedRoute} from "@angular/router";
 import {ServiceService} from "../service.service";
 import {Service} from "../models/service";
 import {Appointment} from "../models/appointment"
+import { MatStepper } from '@angular/material';
 
 @Component({
   selector: 'app-appointment-scheduler',
@@ -17,6 +18,7 @@ export class AppointmentSchedulerComponent implements OnInit {
   selectedDate: Date;
   private appointmentsAtSelectedDate: Appointment[];
 
+  serviceStepName: string = "Velg tjeneste";
 
   private services: Service[] = [];
   private selectedService: Service;
@@ -38,35 +40,13 @@ export class AppointmentSchedulerComponent implements OnInit {
     });
   }
 
-  dateChange(event) {
-    this.selectedDate = event.value._d;
-    this.appointmentService.getAppointmentsByDate(this.selectedDate).subscribe(appointments => {
-      this.appointmentsAtSelectedDate = appointments;
-      this.createTimeArray(this.selectedDate);
-    });
+  goForward(stepper: MatStepper){
+    stepper.next();
   }
 
-  isAvailable(date: Date, service: Service): boolean {
-    let found = this.appointmentsAtSelectedDate.find(appointment => {
-      let end = new Date(appointment.dateTime);
-      end.setMinutes(end.getMinutes() + appointment.durationInMin);
-      return date < appointment.dateTime && date < end;
-    });
-    return found ? true : false;
+  setSelectedService(service: Service){
+    this.selectedService = service;
+    this.serviceStepName = "Valgt tjeneste: " + service.name;
   }
-
-  createTimeArray(chosenDate: Date) {
-    let timeArr: Date[] = [];
-    for (let i = 8; i <= 16; i++) {
-      let date = new Date(chosenDate);
-      date.setHours(i, 0, 0, 0);
-      timeArr.push(date);
-      date = new Date(chosenDate);
-      date.setHours(i, 30,0,0);
-      timeArr.push(date);
-    }
-    this.timeArr = timeArr;
-  }
-
 
 }
--- src/app/date-time-selector/date-time-selector.component.ts
+++ src/app/date-time-selector/date-time-selector.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Appointment} from '../models/appointment';
+import { AppointmentService } from '../appointment.service';
+import { Service } from '../models/service';
 
 @Component({
   selector: 'app-date-time-selector',
@@ -10,27 +13,51 @@ export class DateTimeSelectorComponent implements OnInit {
   private timeArr: Date[] = [];
   private selectedDate: Date;
 
-  constructor() { }
+  @Input()
+  selectedService: Service;
+  
+  private appointmentsAtSelectedDate: Appointment[];
+
+  constructor(private appointmentService: AppointmentService) { }
 
   ngOnInit() {
   }
 
-  dateChange(e: any) {
-    this.createTimeArray(e.value._d);
+  dateChange(event) {
+    this.selectedDate = event.value._d;
+    this.appointmentService.getAppointmentsByDate(this.selectedDate).subscribe(appointments => {
+      console.log(appointments);
+      this.appointmentsAtSelectedDate = appointments;
+      this.createTimeArray(this.selectedDate);
+    });
   }
 
-  createTimeArray(chosenDate: Date){
+  isAvailable(date: Date, service: Service): boolean {
+    let found = this.appointmentsAtSelectedDate.find(appointment => {
+      let x1 = date;
+      let x2 = new Date(date);
+      x2.setMinutes(x1.getMinutes() + service.durationMin);
+
+
+      let y1 =new Date(appointment.dateTime.toDate());
+      let y2 = new Date(appointment.dateTime.toDate());
+      y2.setMinutes(y1.getMinutes() + appointment.durationInMin);
+
+      let isOverlap = (x1 < y2 && y1 < x2);
+      return !isOverlap;
+    });
+
+    return found ? true : false;
+  }
+
+createTimeArray(chosenDate: Date) {
     let timeArr: Date[] = [];
-    for(let i = 8; i<=16; i++){
+    for (let i = 8; i <= 16; i++) {
       let date = new Date(chosenDate);
-      date.setHours(i);
-      date.setMinutes(0);
-      date.setSeconds(0);
+      date.setHours(i, 0, 0, 0);
       timeArr.push(date);
       date = new Date(chosenDate);
-      date.setHours(i);
-      date.setMinutes(30);
-      date.setSeconds(0);
+      date.setHours(i, 30,0,0);
       timeArr.push(date);
     }
     this.timeArr = timeArr;

7f33ae23ea1e9e61b7db8a1f8604bb2599cbfa46
Thomas Wingsternes <twi@ambita.com>
2018-08-19 16:19:11 +0200

    Nå da

--- src/app/appointment-scheduler/appointment-scheduler.component.ts
+++ src/app/appointment-scheduler/appointment-scheduler.component.ts
@@ -3,6 +3,7 @@ import {AppointmentService} from '../appointment.service';
 import {ActivatedRoute} from "@angular/router";
 import {ServiceService} from "../service.service";
 import {Service} from "../models/service";
+import {Appointment} from "../models/appointment"
 
 @Component({
   selector: 'app-appointment-scheduler',
@@ -14,6 +15,8 @@ export class AppointmentSchedulerComponent implements OnInit {
   timeArr: Date[];
 
   selectedDate: Date;
+  private appointmentsAtSelectedDate: Appointment[];
+
 
   private services: Service[] = [];
   private selectedService: Service;
@@ -22,7 +25,6 @@ export class AppointmentSchedulerComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.appointmentService.getAppointment("2");
     this.serviceService.getServices().subscribe(services => {
       this.services = services;
       this.route.params.subscribe(params => {
@@ -37,21 +39,30 @@ export class AppointmentSchedulerComponent implements OnInit {
   }
 
   dateChange(event) {
-    this.createTimeArray(event.value._d);
+    this.selectedDate = event.value._d;
+    this.appointmentService.getAppointmentsByDate(this.selectedDate).subscribe(appointments => {
+      this.appointmentsAtSelectedDate = appointments;
+      this.createTimeArray(this.selectedDate);
+    });
+  }
+
+  isAvailable(date: Date, service: Service): boolean {
+    let found = this.appointmentsAtSelectedDate.find(appointment => {
+      let end = new Date(appointment.dateTime);
+      end.setMinutes(end.getMinutes() + appointment.durationInMin);
+      return date < appointment.dateTime && date < end;
+    });
+    return found ? true : false;
   }
 
   createTimeArray(chosenDate: Date) {
     let timeArr: Date[] = [];
     for (let i = 8; i <= 16; i++) {
       let date = new Date(chosenDate);
-      date.setHours(i);
-      date.setMinutes(0);
-      date.setSeconds(0);
+      date.setHours(i, 0, 0, 0);
       timeArr.push(date);
       date = new Date(chosenDate);
-      date.setHours(i);
-      date.setMinutes(30);
-      date.setSeconds(0);
+      date.setHours(i, 30,0,0);
       timeArr.push(date);
     }
     this.timeArr = timeArr;
--- src/app/appointment.service.ts
+++ src/app/appointment.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestore} from "angularfire2/firestore";
 import {Observable} from "rxjs/index";
-import {Appointment} from "./models/Appointment";
+import {Appointment} from "./models/appointment";
 
 @Injectable({
   providedIn: 'root'
